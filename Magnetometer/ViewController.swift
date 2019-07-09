501345c723c62d87130b60d941c68ede904c4787
Eldrian <andieldrian@gmail.com>
2019-07-09 17:51:38 +0700

    Initial Commit

---
+++ Magnetometer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magnetometer
+//
+//  Created by Andi Ikhsan Eldrian on 09/07/19.
+//  Copyright © 2019 Andi Ikhsan Eldrian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
